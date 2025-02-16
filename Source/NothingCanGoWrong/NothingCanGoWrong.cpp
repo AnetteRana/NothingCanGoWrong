f22ec745b78484db2a9ed97b359bcfa9b943e736
Anette Rana <pertrosfoliea@gmail.com>
2025-02-16 19:16:37 +0100

    Initial project files

---
+++ Source/NothingCanGoWrong/NothingCanGoWrong.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NothingCanGoWrong.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NothingCanGoWrong, "NothingCanGoWrong" );
